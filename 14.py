d947d59c83bac57a28f8ed8c74df674bf44e2fc6
Aleksandr Ponimaskin <aleksandr.ponimaskin@zalando.de>
2021-12-14 09:19:33 +0100

    bruteforce np arrays

--- 14.py
+++ 14.py
@@ -1,7 +1,8 @@
 from pprint import pprint
 from collections import *
 from itertools import *
-from array import array
+import numpy as np
+
 
 # from copy import deepcopy
 # from bisect import bisect_left
@@ -10,16 +11,17 @@ fname = "test.txt"
 # fname = "in/14.txt"
 
 with open(fname) as f:
-    polymer = array("B", [ord(x) for x in f.readline().strip()])
+    polymer = np.array([ord(x) for x in f.readline().strip()])
     _ = f.readline()
     rules = {}
     for l in f:
         i, o = l.strip().split(" -> ")
         rules[tuple([ord(x) for x in i])] = ord(o)
 
-pprint(rules)
+# pprint(rules)
+
 for _ in range(40):
-    new_polymer = array("B", [1 for _ in range(len(polymer) * 2 - 1)])
+    new_polymer = np.empty(polymer.size * 2 - 1, dtype=polymer.dtype)
     for i, pair in enumerate(pairwise(polymer)):
         new_polymer[i * 2] = polymer[i]
         new_polymer[i * 2 + 1] = rules[pair]
