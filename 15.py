92f8173c930aa6a23f0c251f4606b6bf1f4128f5
Aleksandr Ponimaskin <aleksandr.ponimaskin@zalando.de>
2021-12-16 20:35:33 +0100

    day 15

--- 15.py
+++ 15.py
@@ -3,6 +3,8 @@ from collections import *
 from itertools import *
 from heapq import *
 
+from math import inf
+
 fname = "test.txt"
 fname = "in/15.txt"
 
@@ -10,42 +12,65 @@ fname = "in/15.txt"
 with open(fname) as f:
     cave = [[int(i) for i in l.strip()] for l in f]
 
-BOLD = "\033[1m"
-END = "\033[0m"
+_cave = []
+big_cave = []
+for l in cave:
+    ll = []
+    for i in range(0, 5):
+        ll.extend((x + i) % 9 or (x + i) for x in l)
+    _cave.append(ll)
 
+for i in range(0, 5):
+    ll = []
+    for l in _cave:
+        ll.append([(x + i) % 9 or (x + i) for x in l])
+    big_cave.extend(ll)
 
-def pretty(path):
-    pth = set(path)
-    for y, l in enumerate(cave):
-        p = ""
-        for x, s in enumerate(l):
-            if (x, y) in pth:
-                p += BOLD + str(s) + END
-            else:
-                p += str(s)
-        print(p)
-
-
+cave = big_cave
 start = (0, 0)
 
-paths = [(0, (start,))]
+distances = [[inf for _ in cave[0]] for _ in cave]
+distances[0][0] = 0
+visited = set()
 
-end = (len(cave[0]) - 1, len(cave) - 1)
+q = [(0, start)]
 
+while q:
+    d, (x, y) = heappop(q)
 
-while True:
-    weight, path = heappop(paths)
-    x, y = path[-1]
+    dx = x + 1
+    ddx = x - 1
+    dy = y + 1
+    ddy = y - 1
 
-    if (x, y) == end:
-        pretty(path)
-        print()
-        print(weight)
-        break
+    ngb = []
 
-    dx, dy = x + 1, y + 1
+    for nx, ny in [(dx, y), (x, dy), (ddx, y), (x, ddy)]:
+        if (nx, ny) in visited:
+            continue
+        if nx >= len(cave[0]) or nx < 0:
+            continue
+        if ny >= len(cave) or ny < 0:
+            continue
+        # print(nx, ny)
 
-    if dx <= end[0]:
-        heappush(paths, (weight + cave[y][dx], (*path, (dx, y))))
-    if dy <= end[1]:
-        heappush(paths, (weight + cave[dy][x], (*path, (x, dy))))
+        nv = cave[ny][nx]
+        ngb.append((nv, (nx, ny)))
+
+    # ngb.sort(key=lambda x: [0])
+
+    for w, v in ngb:
+        dw = w + d
+        if distances[v[1]][v[0]] > dw:
+            distances[v[1]][v[0]] = dw
+            heappush(q, (dw, v))
+
+    visited.add((x, y))
+
+# pprint(cave[-2][-10:])
+# pprint(cave[-1][-10:])
+
+# pprint(distances[-2][-10:])
+# pprint(distances[-1][-10:])
+# pprint(distances)
+print(distances[-1][-1])
